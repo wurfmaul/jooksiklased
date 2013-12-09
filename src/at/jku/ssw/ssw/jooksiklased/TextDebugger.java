522af4bf694f5ae1a08e56d00e9414a8edd65273
wurfmaul <wurfmaul@posteo.at>
2013-12-09 15:10:53 +0100

    breakpoints work, using dirty hacks :)

--- src/at/jku/ssw/ssw/jooksiklased/TextDebugger.java
+++ src/at/jku/ssw/ssw/jooksiklased/TextDebugger.java
@@ -6,7 +6,9 @@ import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.StringTokenizer;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import com.sun.jdi.ArrayReference;
 import com.sun.jdi.Bootstrap;
@@ -19,6 +21,7 @@ import com.sun.jdi.ReferenceType;
 import com.sun.jdi.StackFrame;
 import com.sun.jdi.StringReference;
 import com.sun.jdi.ThreadReference;
+import com.sun.jdi.VMDisconnectedException;
 import com.sun.jdi.Value;
 import com.sun.jdi.VirtualMachine;
 import com.sun.jdi.connect.AttachingConnector;
@@ -35,14 +38,26 @@ import com.sun.jdi.request.BreakpointRequest;
 import com.sun.jdi.request.EventRequestManager;
 import com.sun.jdi.request.MethodEntryRequest;
 import com.sun.jdi.request.StepRequest;
+import com.sun.jdi.request.VMDeathRequest;
 
+@SuppressWarnings("unused")
 public class TextDebugger {
+	/**
+	 * If an operation requires an unloaded class to be loaded, the operations
+	 * can be stored in this list in order to perform immediately after class
+	 * loading.
+	 */
+	private final Queue<String> pendingOperations;
 	private final VirtualMachine vm;
 	private final EventRequestManager reqManager;
+
 	private ThreadReference curThread;
+	private int countBreakpoints = 0;
 
 	private TextDebugger() throws IOException,
 			IllegalConnectorArgumentsException {
+		pendingOperations = new ConcurrentLinkedQueue<>();
+
 		AttachingConnector con = null;
 		Iterator<Connector> iter = Bootstrap.virtualMachineManager()
 				.allConnectors().iterator();
@@ -71,10 +86,21 @@ public class TextDebugger {
 		}
 	}
 
-	private void run() {
-		vm.resume();
-		curThread.resume();
+	private void cont() {
+		if (!isLoaded()) {
+			System.out.println("Command 'cont' is not valid until the VM is "
+					+ "started with the 'run' command");
+			return;
+		}
 
+		// if there are no more breakpoints
+		if (countBreakpoints == 0) {
+			vm.dispose();
+		} else {
+			curThread.resume();
+			vm.resume();
+		}
+		
 		EventQueue q = vm.eventQueue();
 
 		try {
@@ -83,26 +109,42 @@ public class TextDebugger {
 				EventIterator iter = events.eventIterator();
 				while (iter.hasNext()) {
 					Event e = iter.nextEvent();
+					System.out.println(e);
+
 					if (e instanceof BreakpointEvent) {
 						vm.suspend();
 						BreakpointEvent be = (BreakpointEvent) e;
-						System.out.println("reached breakpoint at "
-								+ be.location().lineNumber() + " in "
-								+ be.location().method().toString());
-						printVars(be.thread().frame(0));
+
+						System.out.printf("Breakpoint hit: \"thread=%s\", %s, "
+								+ "line=%d bci=%d\n", be.thread(), be
+								.location().method(), be.location()
+								.lineNumber(), 0);
+
+						// printVars(be.thread().frame(0));
+						countBreakpoints--;
+						return;
 					} else {
+						System.out.println(e);
 						vm.resume();
 					}
 				}
 			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		} catch (IncompatibleThreadStateException e) {
-			e.printStackTrace();
+			// } catch (IncompatibleThreadStateException e) {
+			// e.printStackTrace();
+		} catch (VMDisconnectedException e) {
+			System.out.println("The application exited.");
 		}
 	}
 
-	private void methodEntry() {
+	private void run() {
+		if (isLoaded()) {
+			System.out
+					.println("VM already running. Use 'cont' to continue after events.");
+			return;
+		}
+
 		try {
 			MethodEntryRequest req = reqManager.createMethodEntryRequest();
 			req.addClassFilter("Test");
@@ -121,9 +163,13 @@ public class TextDebugger {
 					Event e = iter.nextEvent();
 					if (e instanceof MethodEntryEvent) {
 						vm.suspend();
+						while (pendingOperations.size() > 0) {
+							perform(pendingOperations.remove());
+						}
 						return;
 					} else {
 						vm.resume();
+						curThread.resume();
 					}
 				}
 			}
@@ -140,13 +186,13 @@ public class TextDebugger {
 				EventRequestManager reqManager = vm.eventRequestManager();
 				BreakpointRequest req = reqManager.createBreakpointRequest(loc);
 				req.enable();
+				countBreakpoints++;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	@SuppressWarnings("unused")
 	private void stepOver(ThreadReference thread) {
 		try {
 			StepRequest req = reqManager.createStepRequest(thread,
@@ -191,7 +237,6 @@ public class TextDebugger {
 		}
 	}
 
-	@SuppressWarnings("unused")
 	private static void printLocation(Location loc) {
 		System.out.println(loc.lineNumber() + ", " + loc.codeIndex());
 	}
@@ -204,8 +249,11 @@ public class TextDebugger {
 		case "run":
 			run();
 			break;
-			
+
 		case "cont":
+			cont();
+			break;
+
 		case "print":
 		case "dump":
 		case "threads":
@@ -214,39 +262,75 @@ public class TextDebugger {
 			throw new UnsupportedOperationException();
 
 		case "stop":
-			if (st.nextToken().equals("in")) {
+			switch (st.nextToken()) {
+			case "in":
+				// e.g. "stop in Test.hello"
 				final String className = st.nextToken(".").trim();
 				final String methodName = st.nextToken().trim();
-				System.out.printf("wants to stop in %s.%s\n", className,
-						methodName);
 
 				try {
-					assert curThread.frameCount() == 1;
+					if (isLoaded()) {
+						// class is already loaded
+						// find class
+						final List<ReferenceType> classes = vm
+								.classesByName(className);
 
-					// find class
-					final List<ReferenceType> classes = vm
-							.classesByName(className);
-					assert classes.size() == 1;
-					final ReferenceType clazz = classes.get(0);
+						assert classes.size() == 1;
+						final ReferenceType clazz = classes.get(0);
 
-					// find method
-					final List<Method> methods = clazz
-							.methodsByName(methodName);
-					assert methods.size() == 1;
-					final Method method = methods.get(0);
+						// find method
+						final List<Method> methods = clazz
+								.methodsByName(methodName);
+						assert methods.size() <= 1;
+						if (methods.size() == 1) {
+							final Method method = methods.get(0);
 
-					// get first executable line
-					final List<Location> locs = method.allLineLocations();
-					assert locs.size() > 0;
-					final Location loc = locs.get(0);
+							// get first executable line
+							final List<Location> locs = method
+									.allLineLocations();
+							assert locs.size() > 0;
+							final Location loc = locs.get(0);
 
-					System.out.println("Set breakpoint in '" + method
-							+ "' at line " + loc.lineNumber());
+							System.out.println("Set breakpoint in '" + method
+									+ "' at line " + loc.lineNumber());
 
-					setBreakpoint(method, loc.lineNumber());
+							// if pc is already at breakpoint
+							if (curThread.frame(0).location().equals(loc)) {
+								System.out.printf("Breakpoint hit: \"thread="
+										+ "%s\", %s, line=%d bci=%d\n",
+										curThread, method, loc.lineNumber(), 0);
+
+								// vm.suspend();
+							} else {
+								setBreakpoint(method, loc.lineNumber());
+							}
+						} else {
+							System.out
+									.println("Unable to set deferred breakpoint "
+											+ className
+											+ "."
+											+ methodName
+											+ " : No method "
+											+ methodName
+											+ " in " + className);
+						}
+
+					} else {
+						// class is not yet loaded
+						System.out.println("Deferring breakpoint " + className
+								+ "." + methodName + ".");
+						System.out
+								.println("It will be set after the class is loaded.");
+						pendingOperations.add(cmd);
+					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+				break;
+
+			case "at":
+			default:
+				throw new UnsupportedOperationException();
 			}
 			break;
 
@@ -272,6 +356,19 @@ public class TextDebugger {
 				System.err.print(", " + st.nextToken());
 			System.err.println();
 		}
+	}
+
+	/**
+	 * True if VM is loaded, false otherwise.
+	 */
+	private boolean isLoaded() {
+		try {
+			return curThread.frameCount() > 0;
+		} catch (IncompatibleThreadStateException e) {
+			System.err.println("isLoaded() threw "
+					+ e.getClass().getSimpleName());
+		}
+		return false;
 	}
 
 	private int ui() {
@@ -308,7 +405,7 @@ public class TextDebugger {
 	public static void main(String[] arguments) {
 		try {
 			TextDebugger debugger = new TextDebugger();
-			debugger.methodEntry();
+			// debugger.methodEntry();
 			debugger.ui();
 		} catch (IOException | IllegalConnectorArgumentsException e) {
 			e.printStackTrace();
